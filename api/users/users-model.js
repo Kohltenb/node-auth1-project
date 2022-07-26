0f163fbc136acf252b34445ea1fd3780b451142e
Kohlten Bills <kohltenbillsy@gmail.com>
2022-07-26 09:42:05 -0600

    wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -4,14 +4,14 @@ const db = require('../../data/db-config')
   resolves to an ARRAY with all users, each user having { user_id, username }
  */
 function find() {
-  return db('users')
+  return db('users').select('user_id', 'username')
 }
 
 /**
   resolves to an ARRAY with all users that match the filter condition
  */
 function findBy(filter) {
-  return db('users').where(filter)
+  return db('users').select('user_id', 'username').where(filter)
 }
 
 /**
